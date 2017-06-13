8f36dc02b9e80c43d3c55bda83b1f8bf2835cc26
Julio Berina <julioberina@gmail.com>
2017-06-13 00:12:55 -0700

    Allowed spaceship movement with added boundaries

--- src/spaceship.rb
+++ src/spaceship.rb
@@ -1,13 +1,20 @@
 class Spaceship
   def initialize
     @image = Gosu::Image.new("assets/images/spaceship.png")
+    @x = 360
   end
 
   def update
+    move :left if Gosu.button_down? Gosu::KbA
+    move :right if Gosu.button_down? Gosu::KbD
+  end
 
+  def move dir
+    @x -= 5 if dir == :left and @x >= 0
+    @x += 5 if dir == :right and @x <= 725
   end
 
   def draw
-    @image.draw(360, 525, 1, 0.25, 0.25)
+    @image.draw(@x, 525, 1, 0.25, 0.25)
   end
 end
